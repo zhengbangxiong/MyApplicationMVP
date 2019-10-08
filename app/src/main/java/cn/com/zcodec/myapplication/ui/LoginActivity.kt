4686bffdb85d608de7dfc9eb4e7dc69c08b83863
zhengbangxiong <1060417243@qq.com>
2019-10-08 15:38:24 +0800

    author:zhengbangxiong

--- app/src/main/java/cn/com/zcodec/myapplication/ui/LoginActivity.kt
+++ app/src/main/java/cn/com/zcodec/myapplication/ui/LoginActivity.kt
@@ -48,6 +48,7 @@ class LoginActivity : BaseActivity(), LoginContract.View, View.OnClickListener {
 //        setContentView(R.layout.activity_login)
 
         /**
+         * @author
          * databinding
          */
         loginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
